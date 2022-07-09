0a5fb9cef5576932fce5f3df0a17c28ab016468a
lena06051981be <lena06051981be@gmail.com>
2022-07-09 21:40:27 +0300

    up

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    // окно не скролится
+    body: document.querySelector("body"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    // окно не скролится
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
